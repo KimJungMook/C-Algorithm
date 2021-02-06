f42a6cce7283590c7095565c77175c264329a8eb
wjdanr0869@naver.com <wjdanr0869@naver.com>
2021-02-06 15:07:51 +0900

    Algorithm

---
+++ test/KMP Algorithm.cpp
@@ -0,0 +1,36 @@
+/* KMP법 알고리즘  */
+
+int kep_match(const char a[],const char pat[])
+{
+    int pt=1; /* txt 커서 */
+    int pp=0;  /* pat 커서 */
+    int skip[1024]; /* 건너뛰기 표 */
+
+    skip[pt]=0;
+    while(pat[pt]!='\0')
+    {
+        if(pat[pt]==pat[pp])
+            skip[++pt]=++pp;
+        else if(pp==0)
+            skip[++pt]=pp;
+        else
+            pp=skip[pp];
+    } 
+    pt=pp=0;
+    while(txt[pt]!='\0' && pat[pp]!='\0')
+    {
+        if(txt[pt]==pat[pp])
+        {
+            pt++;
+            pp++;
+        }
+        else if(pp==0)
+            pt++;
+        else
+            pp=skip[pp];
+    }
+    if(pat[pp]=='\0')
+        return pt-pp;
+    
+    return -1;
+}
